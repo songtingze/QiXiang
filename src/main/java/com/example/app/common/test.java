0c7b46e6740b7bfb0ba07e44b4dd94532783ed95
songtingze <18301135@bjtu.edu.cn>
2022-07-05 15:35:21 +0800

    添加用户默认头像

---
+++ src/main/java/com/example/app/common/test.java
@@ -0,0 +1,52 @@
+package com.example.app.common;
+
+import cma.cimiss.RetArray2D;
+import cma.cimiss.client.DataQueryClient;
+import com.example.app.demo.ClibUtil;
+
+import java.util.HashMap;
+
+public class test {
+    public static void main(String[] args){
+        DataQueryClient client = new DataQueryClient();
+//        String userId = "DZQX_QXT";
+//        String pwd = "dzqxt@6600";
+        String userId = "BEJN_DZQX_QXT";
+        String pwd = "123456";
+
+        String interfaceId = "getSurfEleByTime";
+
+        HashMap<String,String> params = new HashMap<String,String>();
+
+        params.put("dataCode","SURF_CHN_MUL_HOR");
+        params.put("elements","Station_ID_C,PRE_1h,PRS,RHU,VIS,WIN_S_Avg_2mi,WIN_D_Avg_2mi,Q_PRS");
+
+        params.put("times","20210917000000");
+        params.put("orderby","Station_ID_C:ASC");
+        params.put("limitCnt","10");
+
+        RetArray2D retArray2D = new RetArray2D();
+
+        try {
+            //初始化接口服务连接资源
+            client.initResources() ;
+            //调用接口
+            int rst = client.callAPI_to_array2D(userId, pwd, interfaceId, params, retArray2D) ;
+            //输出结果
+            if(rst == 0) { //正常返回
+                ClibUtil clibUtil = new ClibUtil() ;
+                clibUtil.outputRst( retArray2D ) ;
+            } else { //异常返回
+                System.out.println( "[error] StaElemSearchAPI_CLIB_callAPI_to_array2D." ) ;
+                System.out.printf( "\treturn code: %d. \n", rst ) ;
+                System.out.printf( "\terror message: %s.\n", retArray2D.request.errorMessage ) ;
+            }
+        } catch (Exception e) {
+            //异常输出
+            e.printStackTrace() ;
+        } finally {
+            //释放接口服务连接资源
+            client.destroyResources() ;
+        }
+    }
+}
